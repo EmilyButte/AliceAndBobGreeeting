1bff9287f8cf2971c4d2dbb848a4c28202ef2457
Emily Butte <emily.butte@gmail.com>
2016-10-07 06:51:48 -0400

    completed assignment

--- src/main/java/butte/emily/AliceAndBobGreeting/AliceAndBobGreeting.java
+++ src/main/java/butte/emily/AliceAndBobGreeting/AliceAndBobGreeting.java
@@ -1,30 +1,30 @@
 package butte.emily.AliceAndBobGreeting;
-// Write a program that asks the user for his/her name and only the users Alice and Bob are greeted with their names.
+// Write a program that asks the user for his/her
+//name and only the users Alice and Bob are greeted with their names.
 import java.util.Scanner;
 
 public class AliceAndBobGreeting {
+
     String userName;
 
     public void userName() {
-        //create scanner object to ask user for name
-        System.out.println("What is your name? ");
-        Scanner myScanner = new Scanner(System.in);
-        userName = myScanner.nextLine();
+        System.out.println("What is your name?");
+        Scanner scanner = new Scanner(System.in);
+        userName = scanner.nextLine();
     }
 
     public void loop() {
-        // use if statement to check if user name is Bob or Alice
-        if ((userName.equals("Bob")) || (userName.equals("Alice"))) {
+        if (userName.equals("Bob") || (userName.equals("Alice"))) {
             System.out.println("Hello " + userName);
-        // if not print just hello
         } else {
-            System.out.println("Hello");
+            System.out.println("Hello.");
         }
     }
 
     public static void main(String[] args) {
-        AliceAndBobGreeting aliceAndBobGreeting = new AliceAndBobGreeting();
-        aliceAndBobGreeting.userName();
-        aliceAndBobGreeting.loop();
+        AliceAndBobGreeting greeting = new AliceAndBobGreeting();
+        greeting.userName();
+        greeting.loop();
     }
 }
+
